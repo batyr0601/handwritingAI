af37dc71e7883299c12a7241a1f70049d1fd6ac8
Fumbledumble <niravkoley@gmail.com>
2021-04-05 18:21:49 -0500

    Changed model

--- main/main.py
+++ main/main.py
@@ -7,20 +7,30 @@ import os
 mnist = tf.keras.datasets.mnist # Gets mnist data set
 (x_train, y_train), (x_test, y_test) = mnist.load_data() # Loads data into training and test sets
 
-# Normalize training features from 256-bit color to binary
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# Reshape data to  fit the model
+x_test = x_test.reshape(10000,28,28,1)
+x_train = x_train.reshape(60000,28,28,1)
+
+# Categorize labels by one-hot encoding
+y_test = tf.keras.utils.to_categorical(y_test)
+y_train = tf.keras.utils.to_categorical(y_train)
+print(y_train[0])
 
 model = tf.keras.models.Sequential() # Defines type of neural network
-model.add(tf.keras.layers.Flatten(input_shape=(28,28))) # Add input layer (Take images as an array of 28,28 datapoints)
-# Add hidden layer
-model.add(tf.keras.layers.Dense(units=192, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(units=10, activation = tf.nn.softmax)) # Add output layer
+model.add(tf.keras.layers.Conv2D(32, 3, strides=(2,2), activation=tf.nn.relu, input_shape=(28,28,1)))
+model.add(tf.keras.layers.MaxPool2D(2))
+model.add(tf.keras.layers.Conv2D(64, 3, activation=tf.nn.relu))
+model.add(tf.keras.layers.MaxPool2D(2))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(64, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-history = model.fit(x_train,y_train, epochs=5, validation_data=(x_test,y_test)) # Declares what data the model will use
+history = model.fit(x_train,y_train, epochs=10, validation_data=(x_test,y_test)) # Declares what data the model will use
 
 # Plot loss
 plt.figure()
