538d4206d40603160b8c89a8090e349df41d125b
Fumbledumble <niravkoley@gmail.com>
2021-04-05 19:41:54 -0500

    Added thing

--- main/main.py
+++ main/main.py
@@ -43,12 +43,18 @@ plt.plot(history.history['val_loss'], label ='val loss')
 plt.legend()
 plt.show()
 
-img = cv.imread('5.png')
-img = np.array([img])
-prediction = model.predict(img[0])
-print(np.argmax(prediction))
-plt.imshow(img[0])
-plt.show()
-
-
+while(True): # Input loop for numbers
+    path = input("Absolute path of 28x28 number 0-9 (EXIT to exit): ")
+    if(path == 'EXIT'):
+        break
+    else:
+        path = path.replace(os.sep,'/') # Fix bug with file paths
+        img = cv.imread(path)
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = np.array([img])
+        img = img.reshape(1,28,28,1)
+        prediction = model.predict(img)
+        print(prediction)
+        plt.imshow(img[0])
+        plt.show()
 
