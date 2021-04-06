f9502e38748fecd5cb9513bd46b8964dec32a9ad
timtakcs <timur_takhtarov@houston.nae.school>
2021-04-05 19:47:32 -0500

    separating model from img interpreter

---
+++ main/image_interpreter.py
@@ -0,0 +1,22 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+
+model.load('whatever the name is')
+
+while(True): # Input loop for numbers
+    path = input("Absolute path of 28x28 number 0-9 (EXIT to exit): ")
+    if(path == 'EXIT'):
+        break
+    else:
+        path = path.replace(os.sep,'/') # Fix bug with file paths
+        img = cv.imread(path)
+        img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        img = np.array([img])
+        img = img.reshape(1,28,28,1)
+        prediction = model.predict(img)
+        print(prediction)
+        plt.imshow(img[0])
+        plt.show()
