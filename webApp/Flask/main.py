31a280a817e84fa208854f981caff7c3f81c4159
Fumbledumble <niravkoley@gmail.com>
2021-04-09 15:55:54 -0500

    LESS GOO (flask works)

--- webApp/Flask/main.py
+++ webApp/Flask/main.py
@@ -6,7 +6,9 @@ import base64
 import matplotlib.pyplot as plt
 import io
 import numpy as np
-'''
+import urllib
+from binascii import a2b_base64
+import re
 import tensorflow as tf
 
 if ((os.getcwd()).split(os.sep)[-1] == 'handwritingAI'):
@@ -15,7 +17,6 @@ else:
     os.chdir(f'{os.getcwd()}//handwritingAI')
 
 model = tf.keras.models.load_model('model.tflearn')
-'''
 
 app = Flask(__name__)
 
@@ -26,9 +27,20 @@ def home():
 @app.route('/', methods=['POST'])
 def upload():
     file = request.form['my_hidden']
-    print(file)
 
-    return "gg"
+    imgstr = re.search(r'base64,(.*)', file).group(1)
+    image_bytes = io.BytesIO(base64.b64decode(imgstr))
+    im = Image.open(image_bytes)
+    img = Image.new("RGB", im.size, "WHITE")
+    img.paste(im, (0, 0), im)
+    img = cv.cvtColor(np.asarray(img), cv.COLOR_RGB2BGR)
+    img = cv.resize(img,(28,28))
+    img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img = np.invert(np.array([img]))
+    img = img.reshape(1,28,28,1)
+    prediction = model.predict(img)
+
+    return str(np.argmax(prediction))
 
 if __name__ == '__main__':
     app.run(debug=True)
